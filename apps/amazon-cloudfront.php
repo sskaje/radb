b68e1667365221010d301d4aca37b9105c9b9169
sskaje <sskaje@gmail.com>
2016-07-27 22:13:23 +0800

    amazon cloudfront

--- apps/amazon-cloudfront.php
+++ apps/amazon-cloudfront.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sskaje
- * Date: 16/7/27
- * Time: 下午2:02
- */
+$url = 'https://ip-ranges.amazonaws.com/ip-ranges.json';
+$s = json_decode(file_get_contents($url), true);
+foreach ($s['prefixes'] as $bl) {
+    if ($bl['service'] === 'CLOUDFRONT') {
+        echo $bl['ip_prefix'], "\n";
+    }
+}
